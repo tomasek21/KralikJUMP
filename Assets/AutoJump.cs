efdb8b7a1f2e6c842d0b5cc273983d43b84b4c9b
Miroslav Duszek <duszek_miroslav@oauh.cz>
2023-04-25 11:13:01 +0200

    jump

---
+++ Assets/AutoJump.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoJump : MonoBehaviour
+{
+    public float jumpForce = 10f; // Síla skoku
+    public LayerMask groundLayer; // Vrstva povrchu
+    public Transform groundCheck; // Bod pro detekci povrchu
+    public float groundCheckRadius = 0.01f; // Polomìr kruhu pro detekci povrchu
+    public bool isGrounded; // Indikátor, zda je hráè na zemi
+    public GameObject platform;
+
+    private Rigidbody2D rb; // Komponenta Rigidbody2D
+    private bool shouldJump; // Indikátor, zda by mìl hráè skoèit
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+     
+    }
+
+    void FixedUpdate()
+    {   // Detekce povrchu
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Nastavení indikátoru pro skok, pokud je hráè na zemi
+        if (isGrounded)
+        {
+            shouldJump = true;
+        }
+        // Skok, pokud by mìl hráè skoèit
+        if (shouldJump)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            shouldJump = false;
+        }
+    }
+}
